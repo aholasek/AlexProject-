7456223661973ead03cfb04591b7a5a4cca87bfa
aholasek <student8@js-c02mxrmag083.punahou.edu>
2015-06-26 13:57:06 -1000

    +p2 and app fixes

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 
 // python -m SimpleHTTPServer 8000
 
-prompt("Welcome to localhost:8000");
+
+function callback(a){
+    return function(){
+    	prompt("What do you think of this " + a);
+    }
+}
+var a = "website?";
+setTimeout(callback(a), 5000);
+a = "Stac Overflow";
